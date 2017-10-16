411f66b9d0e98bb61784bffe535ab5cc3209032d
ndean87 <ndean87@gmail.com>
2017-10-15 21:45:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
 function getIssues() {
+  const issueRepo = 'ndean87/javascript-fetch-lab/issues';
+  fetch(`https://api.github.com/repos/${issueRepo}`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  const issuesElement = document.getElementById('issues');
+  json.forEach(object => {
+    const div = document.createElement('div');
+    div.innerText = `${object.title}: ${object.body}`;
+    issuesElement.appendChild(div);
+  });
 }
 
 function createIssue() {
+  const titleInput = document.getElementById('title').value;
+  const textInput = document.getElementById('body').value;
+
+  const issueData = {
+    title: titleInput,
+    body: textInput
+  };
+  const issueRepo = 'ndean87/javascript-fetch-lab/issues';
+  fetch(`https://api.github.com/repos/${issueRepo}`, {
+    method: /post/,
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => getIssues());
 }
 
-function showResults(json) {
+function showForkedRepo(json) {
+  const resultsElement = document.getElementById('results');
+  const linkTo = document.createElement('a');
+  linkTo.href = json.html_url;
+  console.log(json.html_url);
+  linkTo.innerHTML = json.html_url;
+  resultsElement.appendChild(linkTo);
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-curriculum/javascript-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: /post/,
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showForkedRepo(json));
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
+  return '';
 }
+
+// example of why you use then
+
+// fetch('https://api.github.com/repos')
+//   .then(res => {
+//     const myDiv = createElement('div');
+//     myDiv.innerHTML = res;
+//   })
